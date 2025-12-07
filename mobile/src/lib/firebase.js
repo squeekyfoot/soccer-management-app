4656630d5092b4e5f479e5cded3c0b2596ddd532
David Broxton <broxtondude@gmail.com>
2025-12-06 20:13:17 -0800

    Remove keys from repository.

--- mobile/src/lib/firebase.js
+++ mobile/src/lib/firebase.js
@@ -6,13 +6,13 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 // PASTE YOUR CONFIG HERE (Same as web/.env or web/src/lib/firebase.js)
 const firebaseConfig = {
-  apiKey: "AIzaSyBI4jzmLya_E9JT6NWBydIRVvSLBi5RKy4",
-  authDomain: "soccer-management-app.firebaseapp.com",
-  projectId: "soccer-management-app",
-  storageBucket: "soccer-management-app.firebasestorage.app",
-  messagingSenderId: "465013767492",
-  appId: "1:465013767492:web:575ed6f193654bd7436b11",
-  measurementId: "G-EQJFZV1LPD"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
